cf911318b452a5190094a47c0bbd31103cdad51d
BHANUJATIN <bjsbainsla@gmail.com>
2021-11-13 11:15:19 +0530

    some edits are made.

--- include/employee.hpp
+++ include/employee.hpp
@@ -1,28 +1,31 @@
 #pragma once
-#ifndef EMPLOYEE_H
-#define EMPLOYEE_H
+#ifndef EMPLOYEE_HPP
+#define EMPLOYEE_HPP
 #include "./customDataTypes.hpp"
 #include "./recognisedPerson.hpp"
-class employee: public recognisedPerson
-{
-protected:
-int employeeId,shopId,supervisorEmployeeId;
-enum category{parttime,fulltime} empcategory;
-enum type{direct,contracted} emptype;
-struct date dateOfJoining;
-public:
-void setemployeeId();
-void setShopId();
-void setSupervisorEmployeeId();
-void setCategory();
-void setType();
-void setEmployeeDetails();
-void getemployeeId();
-void getShopId();
-void getSupervisorEmployeeId();
-void getCategory();
-void getType();
-void getDateOfJoining();
-void getEmployeeDetails();
+class employee : public recognisedPerson {
+    protected:
+    int employeeId, shopId, supervisorEmployeeId;
+    enum category { parttime,
+                    fulltime } empcategory;
+    enum type { direct,
+                contracted } emptype;
+    struct date dateOfJoining;
+
+    public:
+    void setemployeeId();
+    void setShopId();
+    void setSupervisorEmployeeId();
+    void setCategory();
+    void setType();
+    void setEmployeeDetails();
+    void getemployeeId();
+    void getShopId();
+    void getSupervisorEmployeeId();
+    void getCategory();
+    void getType();
+    void getDateOfJoining();
+    void getEmployeeDetails();
+    
 };
 #endif
